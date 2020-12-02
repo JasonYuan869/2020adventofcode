945daaf8759d6b60d55e0ae4de6343dbfab6317f
Jason Yuan <jason.yuan869@gmail.com>
2020-12-02 14:42:55 -0500

    add comment

--- Day 1/Day1.java
+++ Day 1/Day1.java
@@ -1,5 +1,12 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Day1 {
@@ -31,6 +38,7 @@ public class Day1 {
         File expenseInput = new File("input.txt");
         BufferedReader br = new BufferedReader(new FileReader(expenseInput));
         List<Integer> expenseReport = new ArrayList<Integer>();
+        // Convert String stream to Integer List
         expenseReport.addAll(br.lines().map(Integer::valueOf).collect(Collectors.toList()));
         System.out.println(day1(expenseReport));
         System.out.println(day2(expenseReport));
